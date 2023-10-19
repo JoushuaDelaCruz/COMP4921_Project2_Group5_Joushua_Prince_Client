0a107b1b05862e634ace7daeaa4141d3afc51f7f
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-19 09:43:36 -0700

    Implemented the sign up page

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,12 +1,142 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const SignUp = () => {
+  const [errorMsgs, setErrorMsgs] = useState([]);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [isUsernameInvalid, setIsUsernameInvalid] = useState(false);
-  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
   const [password, setPassword] = useState("");
-  const [isPasswordInvalid, setIsPasswordInvalid] = useState(false);
+
+  useEffect(() => {
+    const getErrorMsgs = async () => {
+      const response = await fetch("/assets/user-msgs.json");
+      const data = await response.json();
+      setErrorMsgs(data);
+    };
+    getErrorMsgs();
+  }, []);
+
+  const getElement = (id) => document.getElementById(id);
+
+  const getErrorMsg = (id) => {
+    return errorMsgs[id]["msg"];
+  };
+
+  const _credentialInvalid = (id, errId) => {
+    const invalidElement = getElement(`is-${id}-invalid`);
+    const inputContainer = getElement(id);
+    inputContainer.classList.remove("ring-gray-300");
+    inputContainer.classList.add("ring-red-500");
+    invalidElement.classList.remove("hidden");
+    invalidElement.innerText = getErrorMsg(errId);
+    return false;
+  };
+
+  const _credentialValid = (id) => {
+    const invalidElement = getElement(`is-${id}-invalid`);
+    const inputContainer = getElement(id);
+    inputContainer.classList.remove("ring-gray-300");
+    inputContainer.classList.remove("ring-red-500");
+    inputContainer.classList.add("ring-green-500");
+    invalidElement.classList.add("hidden");
+    return true;
+  };
+
+  const validateEmail = async () => {
+    if (!email) {
+      return _credentialInvalid("email", 1);
+    }
+    const response = await axios.post(
+      import.meta.env.VITE_API + "user/isEmailExist",
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          email: email,
+        },
+      }
+    );
+    if (response.data) {
+      return _credentialInvalid("email", 8);
+    } else {
+      return _credentialValid("email");
+    }
+  };
+
+  const validateUsername = async () => {
+    if (!username) {
+      _credentialInvalid("username", 0);
+      return _credentialInvalid("username", 0);
+    }
+    const response = await axios.post(
+      import.meta.env.VITE_API + "user/isUsernameExist",
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          username: username,
+        },
+      }
+    );
+    if (response.data) {
+      return _credentialInvalid("username", 9);
+    } else {
+      return _credentialValid("username");
+    }
+  };
+
+  const validatePassword = () => {
+    if (password.length < 10) {
+      return _credentialInvalid("password", 2);
+    }
+    if (!password.match(/[a-z]/)) {
+      return _credentialInvalid("password", 3);
+    }
+    if (!password.match(/[A-Z]/)) {
+      return _credentialInvalid("password", 4);
+    }
+    if (!password.match(/[0-9]/)) {
+      return _credentialInvalid("password", 5);
+    }
+    if (!password.match(/[^a-zA-Z0-9]/)) {
+      return _credentialInvalid("password", 6);
+    }
+    return _credentialValid("password");
+  };
+
+  const validateCredentials = async () => {
+    const isUsernameValid = validateUsername();
+    const isEmailValid = validateEmail();
+    const isPasswordValid = validatePassword();
+    if (isUsernameValid && isEmailValid && isPasswordValid) {
+      const data = {
+        username: username,
+        email: email,
+        password: password,
+      };
+      const response = await axios.post(
+        import.meta.env.VITE_API + "user/create",
+        data,
+        {
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data) {
+        window.location.href = "/login";
+      } else {
+        alert("Something went wrong");
+        window.location.reload();
+      }
+    }
+    return;
+  };
 
   return (
     <main className="flex justify-center items-center background">
@@ -14,7 +144,7 @@ const SignUp = () => {
         <div className="w-1/2 lg:border-r-2 border-cyan-600">
           <img
             className="mx-auto w-auto h-24 lg:h-auto"
-            src="./images/logo.png"
+            src="/images/logo.png"
             alt="Your Company"
           />
         </div>
@@ -28,10 +158,7 @@ const SignUp = () => {
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <section className="space-y-5">
               <div>
-                <label
-                  for="username"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   Username
                 </label>
                 <div className="mt-1">
@@ -39,21 +166,17 @@ const SignUp = () => {
                     id="username"
                     name="username"
                     type="email"
+                    onChange={(e) => setUsername(e.target.value)}
                     className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
                 <div
                   id="is-username-invalid"
                   className="pt-1 text-sm text-red-600 hidden"
-                >
-                  Please enter a valid username
-                </div>
+                ></div>
               </div>
               <div>
-                <label
-                  for="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   Email Address
                 </label>
                 <div className="mt-1">
@@ -61,6 +184,7 @@ const SignUp = () => {
                     id="email"
                     name="email"
                     type="text"
+                    onChange={(e) => setEmail(e.target.value)}
                     className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -74,10 +198,7 @@ const SignUp = () => {
 
               <div>
                 <div className="flex items-center justify-between">
-                  <label
-                    for="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
                     Password
                   </label>
                 </div>
@@ -86,8 +207,7 @@ const SignUp = () => {
                     id="password"
                     name="password"
                     type="password"
-                    autocomplete="current-password"
-                    required
+                    onChange={(e) => setPassword(e.target.value)}
                     className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -101,7 +221,7 @@ const SignUp = () => {
 
               <div>
                 <button
-                  onclick="validateCredentials()"
+                  onClick={validateCredentials}
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
