7c16033d231778ce62ff3ccc242b8a0b3a5e4409
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-18 19:37:49 -0700

    Created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Post from "./Pages/Post";
+import LogIn from "./Pages/LogIn";
+import SignUp from "./Pages/SignUp";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/post" exact element={<Post />} />
+        <Route path="/logIn" exact element={<LogIn />} />
+        <Route path="/signUp" exact element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
