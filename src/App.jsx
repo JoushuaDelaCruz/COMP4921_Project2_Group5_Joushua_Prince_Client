194bcf47c59e7954410b860d77af0bb57b8d41cf
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-20 23:18:26 -0700

    Prevented user from accessing post if not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
 import Post from "./Pages/Post";
 import LogIn from "./Pages/LogIn";
 import SignUp from "./Pages/SignUp";
+import { useCookies } from "react-cookie";
+import Request from "./Pages/models/ServerRequest";
 
 const App = () => {
+  const request = new Request();
+  const [cookies] = useCookies(["session"]);
+
+  const isCookieValid = async () => {
+    if (cookies.session) {
+      const url = import.meta.env.VITE_API + "user/checkSession";
+      const authenticated = await request.postReq(url, cookies.session);
+      return authenticated;
+    }
+    return false;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/post" exact element={<Post />} />
-        <Route path="/logIn" exact element={<LogIn />} />
-        <Route path="/signUp" exact element={<SignUp />} />
+        <Route
+          path="/post"
+          exact
+          element={isCookieValid ? <Navigate to="/" replace /> : <Post />}
+        />
+        <Route
+          path="/logIn"
+          exact
+          element={isCookieValid ? <LogIn /> : <Navigate to="/" replace />}
+        />
+        <Route
+          path="/signUp"
+          exact
+          element={isCookieValid ? <SignUp /> : <Navigate to="/" replace />}
+        />
       </Routes>
     </BrowserRouter>
   );
