c62a6c5f0b025ebd2593110962d9eaf52803e795
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-10-22 23:14:56 -0700

    Dynamically hides voting if user not logged in

--- src/Pages/Components/PostCard.jsx
+++ src/Pages/Components/PostCard.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { AdvancedImage } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
+import { Link } from "react-router-dom";
 
-const PostCard = ({ post }) => {
+const PostCard = ({ post, user, isReplyPage }) => {
   const cld = new Cloudinary({
     cloud: { cloudName: import.meta.env.VITE_CLOUD_NAME },
   });
   const myImage = cld.image(post.profile_img);
   return (
-    <div className="flex w-full bg-white h-fit rounded">
+    <div className="flex w-full bg-white h-fit rounded-sm">
       <div className="w-12 flex flex-col items-center content-center bg-blue-100 gap-3 rounded-l">
-        <button className="mt-3 text-green-400">
-          <i className="fa-regular fa-circle-up fa-2xl"></i>
-        </button>
-        <span className="font-semibold text-sm"> {post.num_votes || 0} </span>
-        <button className="text-green-400">
-          <i className="fa-regular fa-circle-down fa-2xl"></i>
-        </button>
+        {user && (
+          <button className="mt-3 text-green-400">
+            <i className="fa-regular fa-circle-up fa-2xl"></i>
+          </button>
+        )}
+        <span className={"font-semibold text-sm " + (user ? "" : "my-3")}>
+          {" "}
+          {post.num_votes || 0}{" "}
+        </span>
+        {user && (
+          <button className="text-green-400">
+            <i className="fa-regular fa-circle-down fa-2xl"></i>
+          </button>
+        )}
       </div>
       <div className="w-full p-1">
-        <a href="/user?=<%= post.user_id %>" className="">
-          <header className="flex w-full gap-2 items-center p-2">
+        <Link to="#" className="w-fit flex">
+          <header className="flex gap-2 items-center p-2 w-fit">
             <AdvancedImage
               cldImg={myImage}
               className="w-8 h-8 rounded-full ring-1 ring-gray-300 overflow-hidden object-cover"
@@ -33,22 +41,32 @@ const PostCard = ({ post }) => {
               </span>{" "}
             </span>
           </header>
-        </a>
+        </Link>
         <div className="p-2">
           <h2 className="text-xl font-semibold mb-3"> {post.title} </h2>
           <p className="text-sm"> {post.content} </p>
         </div>
         <footer className="flex">
-          <a
-            href="/replies?=<%= post.content_id %>"
-            className="flex items-center gap-1 p-2 ml-1 mt-1 rounded-sm text-gray-400 hover:bg-gray-100"
-          >
-            <i className="fa-regular fa-comment fa-lg"></i>
-            <span className="text-xs font-bold">
-              {" "}
-              {post.num_comments} Comments{" "}
-            </span>
-          </a>
+          {isReplyPage ? (
+            <div className="flex items-center gap-1 p-2 ml-1 mt-1 rounded-sm text-gray-400">
+              <i className="fa-regular fa-comment fa-lg"></i>
+              <span className="text-xs font-bold">
+                {" "}
+                {post.num_comments} Comments{" "}
+              </span>
+            </div>
+          ) : (
+            <Link
+              to={"/reply/" + post.content_id}
+              className="flex items-center gap-1 p-2 ml-1 mt-1 rounded-sm text-gray-400 hover:bg-gray-100"
+            >
+              <i className="fa-regular fa-comment fa-lg"></i>
+              <span className="text-xs font-bold">
+                {" "}
+                {post.num_comments} Comments{" "}
+              </span>
+            </Link>
+          )}
           <button className="flex items-center gap-2 p-2 mt-1 rounded-sm text-gray-400 hover:bg-gray-100">
             <i className="fa-regular fa-bookmark fa-lg"></i>
             <span className="text-xs font-bold"> Bookmark </span>
