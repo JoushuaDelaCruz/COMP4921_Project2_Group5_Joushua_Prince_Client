11603d768f1b9562d45a9bd6b591bb5faddce9b0
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-02 22:56:53 -0700

    Removed unused useEffect import

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
 import useUser from "./Models/useUser";
 import Request from "./Models/ServerRequest";
