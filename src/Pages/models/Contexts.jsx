a82bf7e3b964be898c3afc05cc2d8159d9a714ae
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-04 00:10:58 -0700

    Renamed setPostContentHandler

--- src/Pages/models/Contexts.jsx
+++ src/Pages/models/Contexts.jsx
@@ -2,4 +2,4 @@ import { createContext } from "react";
 
 export const RepliesContext = createContext(null);
 export const UserContext = createContext(null);
-export const SetPostsHandlerContext = createContext(null);
+export const EditContentHandlerContext = createContext(null);
