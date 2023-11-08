702d522a6a1b68b991df375fb953f324d3566d65
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-08 12:11:43 -0800

    Added animations

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -15,7 +15,7 @@ const PageNotFound = () => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
-          className="w-40 h-40 dark:text-gray-600"
+          className="w-40 h-40 dark:text-gray-600 sm:w-48 sm:h-48 animate-spin-slow"
         >
           <path
             fill="currentColor"
@@ -37,14 +37,14 @@ const PageNotFound = () => {
             points="383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63"
           ></polygon>
         </svg>
-        <div className="text-3xl flex flex-col gap-2">
+        <div className="text-3xl flex flex-col gap-2 animate-pulse">
           <p>This may not mean anything.</p>
-          <p>We are probably working on that has blown up!</p>
+          <p>We are probably working on something that has blown up!</p>
         </div>
         <Link
           rel="noopener noreferrer"
           to={"/"}
-          className="px-8 py-3 font-bold rounded border border-transparent dark:bg-violet-400 dark:text-gray-900 hover:border-white"
+          className="px-8 py-3 font-bold rounded border border-transparent dark:bg-violet-400 dark:text-gray-900 hover:border-white hover:bg-transparent hover:text-white transition duration-300 delay-100"
         >
           Back to homepage
         </Link>
