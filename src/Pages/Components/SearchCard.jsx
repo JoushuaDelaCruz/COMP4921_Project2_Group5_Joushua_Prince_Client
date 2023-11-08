e492f9e18028589fc038fa906fcd9afb7a0e5ad7
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-08 01:06:05 -0800

    Created the front-end for the Search

---
+++ src/Pages/Components/SearchCard.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import useRequest from "../Models/useRequest";
+
+const SearchCard = ({ content }) => {
+  const cld = new Cloudinary({
+    cloud: { cloudName: import.meta.env.VITE_CLOUD_NAME },
+  });
+  const myImage = cld.image(content.profile_img);
+  const [getRequest] = useRequest();
+
+  const formatDate = (date) => {
+    const dateObj = new Date(date);
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth();
+    const year = dateObj.getFullYear();
+    return `${year}-${month}-${day}`;
+  };
+
+  const formatContent = (content) => {
+    if (content.length > 250) {
+      return content.substring(0, 205) + "...";
+    }
+    return content;
+  };
+
+  const findParent = async () => {
+    console.log("I have been clicked");
+    // Uncomment this out. When you implement it. Also this assumes that the response is the parent_id
+
+    // const url = import.meta.env.VITE_API + "post/getPost/" + content.parent_id;
+    // const response = await getRequest(url);
+    // if (response) {
+    //   window.location.href = "/post/" + response;
+    // }
+    // return response;
+  };
+
+  return (
+    <div
+      className="flex flex-col rounded-sm bg-white px-4 py-2 ring-1 ring-gray-400 hover:ring-gray-600 hover:bg-gray-100 cursor-pointer"
+      onClick={findParent}
+    >
+      <header className="flex justify-between items-center text-gray-500 text-sm py-2">
+        <div className="flex items-center gap-3">
+          <AdvancedImage
+            cldImg={myImage}
+            className="w-12 h-12 rounded-full ring-1 ring-gray-300 overflow-hidden object-cover"
+          />
+          <h1>
+            {" "}
+            Posted by <span className="font-bold">
+              {" "}
+              {content.username}{" "}
+            </span>{" "}
+          </h1>
+        </div>
+        <span className="text-xs">
+          Date Created: {formatDate(content.date_created)}
+        </span>
+      </header>
+      <p className="text-sm py-3">{formatContent(content.content)}</p>
+      <footer className="flex items-center py-2 gap-3"></footer>
+    </div>
+  );
+};
+
+export default SearchCard;
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -2,50 +2,23 @@ import React, { useState, useEffect } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import useUser from "./Models/useUser";
+import SearchCard from "./Components/SearchCard";
 
 const Search = () => {
   const { text } = useParams();
   const textResults = useLoaderData();
-  const [user, setUser] = useState(null);
-  const [profileImg, setProfileImg] = useState("");
-  const cld = new Cloudinary({
-    cloud: { cloudName: import.meta.env.VITE_CLOUD_NAME },
-  });
-
-  useEffect(() => {
-    const getUser = async () => {
-      const url = import.meta.env.VITE_API + "user";
-      const data = await request.postReq(url, cookies.session);
-
-      if (data && data.profile_img) {
-        setUser(data);
-        const image = cld.image(data.profile_img);
-        setProfileImg(image);
-      } else {
-        setUser(null);
-        removeCookie("session");
-      }
-    };
-    getUser();
-  }, []); // Add an empty dependency array to run the effect only once
-
-  // Handle button click to navigate to the "reply" route
-  const handleButtonClick = (parent_id) => {
-    window.location.href = `/reply/${parent_id}`;
-  };
+  const [user, profileImg] = useUser(null);
 
   return (
     <>
       <Navbar user={user} image={profileImg} search={text} />
-      <main className="background">
-        {textResults.map((text) => (
-          <div key={text.parent_id}>
-            <div>{text.content}</div>
-            <button onClick={() => handleButtonClick(text.parent_id)}>
-              Go to main thread
-            </button>
-          </div>
-        ))}
+      <main className="flex justify-center background py-6">
+        <section className="flex flex-col w-1/2 h-fit">
+          {textResults &&
+            textResults.map((content) => {
+              return <SearchCard content={content} />;
+            })}
+        </section>
       </main>
     </>
   );
